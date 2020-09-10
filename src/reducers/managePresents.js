e5a6a982c009ef1505e640db4cbd2db657077a5e
Kelsey Shiba <kelsey.shiba@gmail.com>
2020-09-10 15:03:44 -0700

    Done.

--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,12 @@
 export function managePresents(state, action){
+    // state = {numberOfPresents: 0}
+    // action = {
+    //     type: 'INCREASE'
+    // }
+
+    if (action.type === 'INCREASE'){
+        return {numberOfPresents: state.numberOfPresents + 1}
+    } else {
+        return state
+    }
 }
